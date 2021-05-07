df1257fdf316a68c159bb1436d1503c009065c6e
kubeczki <comjakub@gmail.com>
2021-05-07 22:05:29 +0200

    f git

--- face_detector.py
+++ face_detector.py
@@ -1,6 +1,7 @@
 import cv2
 
 
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Convert to grayscale
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect the faces
